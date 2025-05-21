abe2c2286f12f0fd237a87e84bd42591f4efc181
David Kiki <david.kariuki2@student.moringaschool.com>
2025-05-21 10:24:48 +0300

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+('Mr. Chocolate', 5, 'M', 'brown', 'friendly', 1),
+('Rowdy', 8, 'M', 'black', 'aggressive', 1),
+('Tabitha', 3, 'F', 'white', 'gentle', 1),
+('Sergeant Brown', 10, 'M', 'dark brown', 'strict', 0),
+('Melissa', 4, 'F', 'golden', 'playful', 1),
+('Grinch', 7, 'M', 'green', 'grumpy', 0),
+('Wendy', 6, 'F', 'gray', 'calm', 1),
+(NULL, 2, 'F', 'black', 'shy', 1);
